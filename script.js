e0e25846a84327bb02596b1bc644a55c449d32fd
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-05-18 11:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-//your JS code here. If required.
+const output = document.querySelector("#output");
+
+const inputHandler = () => {
+  const val = document.querySelector("#ip").value;
+  prom(+val);
+};
+
+const prom = async (val) => {
+  const pro = new Promise((resolve) => {
+    setTimeout(() => {
+      output.innerText = `Result: ${val}`;
+      console.log("first", val);
+      resolve(val);
+    }, 2000);
+  });
+
+  pro
+    .then((data) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const mul = data * 2;
+          output.textContent = `Result: ${mul}`;
+          console.log("mul", mul);
+          resolve(mul);
+        }, 1000);
+      });
+    })
+    .then((data) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const sub = data - 3;
+          output.innerText = `Result: ${sub}`;
+          resolve(sub);
+        }, 1000);
+      });
+    })
+    .then((data) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const divide = data / 2;
+          output.innerText = `Result: ${divide}`;
+          resolve(divide);
+        }, 1000);
+      });
+    })
+    .then((data) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const add = data + 10;
+          output.innerText = `Result: ${add}`;
+          resolve(add);
+        }, 1000);
+      });
+    })
+    .then((data) => {
+      output.innerText = `Final Result: ${data}`;
+    });
+};
